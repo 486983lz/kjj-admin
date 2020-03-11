7c12739c98f4f8af0e2ac38a753ed9658c4911ac
renzhuzhu <443839006@qq.com>
2020-03-11 17:31:19 +0800

    '修改模块'

--- src/api/expert.js
+++ src/api/expert.js
@@ -3,21 +3,21 @@ import request from '@/utils/request'
 
 export function createExpert(data) {
   return request({
-    url: '/recommend/expert/createExpert',
+    url: '/admin/expert/createExpert',
     method: 'post',
     data
   })
 }
 export function updateExpert(data) {
   return request({
-    url: '/recommend/expert/updateExpert',
+    url: '/admin/expert/updateExpert',
     method: 'post',
     data
   })
 }
 export function getExpertList(data) {
   return request({
-    url: '/recommend/expert/getExpertList',
+    url: '/admin/expert/getExpertList',
     method: 'post',
     data
   })
@@ -25,7 +25,7 @@ export function getExpertList(data) {
 
 export function deleteExpert(data) {
   return request({
-    url: '/recommend/expert/deleteExpert',
+    url: '/admin/expert/deleteExpert',
     method: 'post',
     data
   })
--- src/api/notice.js
+++ src/api/notice.js
@@ -3,14 +3,14 @@ import request from '@/utils/request'
 
 export function uploadTinymceImg(data) {
   return request({
-    url: '/recommend/notice/uploadTinymceImg',
+    url: '/admin/notice/uploadTinymceImg',
     method: 'post',
     data
   })
 }
 export function postNoticeInfo(data) {
   return request({
-    url: '/recommend/notice/postNoticeInfo',
+    url: '/admin/notice/postNoticeInfo',
     method: 'post',
     data
   })
